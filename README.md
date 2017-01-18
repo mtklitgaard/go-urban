39461ba202f603e45b5c3ff6ee61e41bc1b4f05c
Matt Klitgaard <mtklitgaard@gmail.com>
2017-01-18 15:10:15 -0600

    make examples an option

--- README.md
+++ README.md
@@ -2,7 +2,10 @@
 Simple CLI for GO that looks up words on Urban Dictionary
 
 # Usage
-go urban -word=ball -count=10
+go urban ball
+go urban -examples true ball
+go urban -count=10 ball
+go urban -examples true -count=10 ball
 
 # Purpose
 I want to learn go, I like looking up random words on Urban Dictionary for laughs and I got tired of opening chrome to do this. I figured a CLI tool could be useful in satisfying all three.  
--- urban.go
+++ urban.go
@@ -10,10 +10,12 @@ import (
 
 func main() {
 	countPtr := flag.Int("count", 5, "Number of definitions you would like to see")
+	showExamplesPtr := flag.Bool("examples", false, "Show examples if you want them.")
 	flag.Parse()
 	args := flag.Args()
 	wordFromCommand := args[0]
 	numberOfResults := *countPtr
+	showExamples := *showExamplesPtr
 
 	definitions, _ := api.LookupWordDefinition(wordFromCommand)
 
@@ -26,10 +28,16 @@ func main() {
 			result := definitions.List[index-1]
 			fmt.Printf("\n\nResult: %v \n", index)
 			fmt.Println(result.Definition)
-			fmt.Println("Examples:")
-			fmt.Println(result.Example)
+			printExamples(showExamples, result.Example)
 		}
 	} else {
 		fmt.Println("You got no results")
+	}
+}
+
+func printExamples(showExamples bool, example string) {
+	if showExamples {
+		fmt.Println("Examples:")
+		fmt.Println(example)
 	}
 }
